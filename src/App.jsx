f08de61d86651ae74354797e280c1ba174f238f7
kichichoi102 <kichichoi102@gmail.com>
2023-11-05 10:58:37 -0500

    added loading and loading progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
 import { Canvas } from '@react-three/fiber';
+import { useEffect, useState } from 'react';
 import { Experience } from './components/Experience';
 import { Footer } from './components/Footer';
+import { LoadingScreen } from './LoadingScreen';
 import './App.css';
 
+const maxLoadTime = 5000;
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (loading) {
+      const startTime = Date.now();
+
+      const timer = setInterval(() => {
+        const elapsedTime = Date.now() - startTime;
+        const progressValue = (elapsedTime / maxLoadTime) * 100;
+
+        if (progressValue >= 100) {
+          setProgress(100);
+          setLoading(false);
+          clearInterval(timer);
+        } else {
+          setProgress(progressValue);
+        }
+      }, 100);
+
+      return () => clearInterval(timer);
+    }
+  }, [loading]);
+
   return (
     <>
-      <Canvas
-        camera={{
-          fov: 70,
-          position: [2.3, 1.5, 2.3],
-        }}
-      >
-        <Experience />
-      </Canvas>
-      <Footer />
+      {loading ? (
+        <LoadingScreen progress={progress} />
+      ) : (
+        <>
+          <Canvas
+            camera={{
+              fov: 70,
+              position: [2.3, 1.5, 2.3],
+            }}
+          >
+            {loading ? <Hello progress={progress} /> : <Experience />}
+          </Canvas>
+          <Footer />
+        </>
+      )}
     </>
   );
 }
---
+++ src/LoadingScreen.jsx
@@ -0,0 +1,3 @@
+export const LoadingScreen = ({progress}) => {
+    return <h1>{Math.round(progress)}%</h1>
+}
